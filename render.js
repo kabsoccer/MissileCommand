68b3c3b8efbbae7d99a8ea081d6fa128d511e39c
Kevin Bartushak <kabartus@ncsu.edu>
2017-12-06 16:12:21 -0500

    Creating enemy missiles

--- render.js
+++ render.js
@@ -565,10 +565,10 @@ function loadModels() {
         console.log(e);
     } // end catch
 	
-	enemyMissiles.push([Math.random(), Math.random() * 2 + 1, 0, Math.random() / 100 - (0.5/100)],
-					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 100 - (0.5/100)],
-					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 100 - (0.5/100)],
-					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 100 - (0.5/100)]);
+	enemyMissiles.push([Math.random(), Math.random() * 2 + 1, 0, Math.random() / 1000 - (0.5/1000)],
+					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 1000 - (0.5/1000)],
+					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 1000 - (0.5/1000)],
+					   [Math.random(), Math.random() * 2 + 1, 0, Math.random() / 1000 - (0.5/1000)]);
 } // end load models
 
 // setup the webGL shaders
