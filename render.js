1fa77af5343f41089f04ebbb87a5f4d651cc86fa
Kevin Bartushak <kabartus@ncsu.edu>
2017-12-06 16:02:53 -0500

    Creating enemy missiles

--- render.js
+++ render.js
@@ -854,6 +854,8 @@ function renderModels() {
         
         // draw a transformed instance of the ellipsoid
         gl.drawElements(gl.TRIANGLES,triSetSizes[numTriangleSets+0],gl.UNSIGNED_SHORT,0); // render
+		
+		enemyMissiles[i][2] -= .01;
     } // end for each ellipsoid
 } // end render model
 
